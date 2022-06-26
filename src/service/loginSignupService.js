4eb4d7534c0c527f6b024e4945e70106cc9eab92
Phinv <nguyenvanphidmx123455@gmail.com>
2022-06-26 19:26:48 +0700

    done

---
+++ src/service/loginSignupService.js
@@ -0,0 +1,18 @@
+import api from "../api";
+
+
+
+export const login = async (user) => {
+    return fetch(`http://localhost:8080/api/v1/users/login/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: user,
+    })
+  };
+
+export const signup = async (newUser) => {
+      return api.post(`/users`, newUser)
+  }
+export default {login, signup}
